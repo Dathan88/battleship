f0ac3a7f76e5981d30ba5d44f2a3797b70ef7f26
Dathan Spencer <dpspencer88@gmail.com>
2018-12-14 16:01:54 -0700

    Create Game module file and test file

---
+++ src/Components/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Game extends Component {
+	render() {
+		return (
+			<div className='Game'>
+				<header className='Game-header'>Battleship</header>
+			</div>
+		);
+	}
+}
