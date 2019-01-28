eeb8c2922bea286260421b10e5095c2b9d238848
Dathan Spencer <dpspencer88@gmail.com>
2019-01-28 16:01:38 -0700

    Updates

---
+++ precache-manifest.fd9be1409346689facd8e920a718794d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71f5fd5526bd984b7aed",
+    "url": "/battleship/static/js/runtime~main.71f5fd55.js"
+  },
+  {
+    "revision": "efca71267a7c3bef9aaf",
+    "url": "/battleship/static/js/main.efca7126.chunk.js"
+  },
+  {
+    "revision": "95bf6d79e70717aac3ee",
+    "url": "/battleship/static/js/1.95bf6d79.chunk.js"
+  },
+  {
+    "revision": "efca71267a7c3bef9aaf",
+    "url": "/battleship/static/css/main.6d71b607.chunk.css"
+  },
+  {
+    "revision": "d9332c17410bf5f3f01171f2415a9995",
+    "url": "/battleship/index.html"
+  }
+];
